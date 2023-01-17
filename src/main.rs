03862197964912efd4e44631252b32b4acc8242c
kdan80 <kierandansey80@gmail.com>
2023-01-17 19:09:44 +0000

    Finished 13.1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use std::env;
-use std::process;
-use minigrep::Config;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
 
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
 }
