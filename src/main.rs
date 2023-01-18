dd2daa08bacf280df8db684918ed3c11851d975f
kdan80 <kierandansey80@gmail.com>
2023-01-18 18:01:31 +0000

    Basic actix webserver

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello kieran")
 }
 
-fn main() {
-    let mut list = [
-        Rectangle { width: 10, height: 1 },
-        Rectangle { width: 3, height: 5 },
-        Rectangle { width: 7, height: 12 },
-    ];
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
-    list.sort_by_key(|r| r.width);
-    println!("{:#?}", list);
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
